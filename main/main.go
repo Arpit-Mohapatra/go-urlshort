4b70604daebf37bea531ca6c7404eff287e527cc
Arpit Mohapatra <mohapatra.arpit2003@cet.edu.in>
2023-08-04 02:53:48 +0530

    main function tested for maphandle

--- main/main.go
+++ main/main.go
@@ -3,10 +3,19 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/Arpit-Mohapatra/urlshort"
 )
 
 func main() {
+	mux := defaultMux()
+	pathsToUrls := map[string]string{
+		"/urlshort-godoc": "https://godoc.org/github.com/gophercises/urlshort",
+		"/yaml-godoc":     "https://godoc.org/gopkg.in/yaml.v2",
+	}
+	mapHandler := urlshort.MapHandler(pathsToUrls, mux)
 	fmt.Println("Starting server on port 8080")
+	http.ListenAndServe(":8080", mapHandler)
 }
 
 func defaultMux() *http.ServeMux {
