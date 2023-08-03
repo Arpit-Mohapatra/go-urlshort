9c873b89456cce706f3c1c233c0dcc1195c9a2cc
Arpit Mohapatra <mohapatra.arpit2003@cet.edu.in>
2023-08-04 02:39:58 +0530

    MapHandler function added

--- handler.go
+++ handler.go
@@ -1,9 +1,16 @@
 package urlshort
 
-import "net/http"
+import (
+	"net/http"
+)
 
 func MapHandler(pathToUrls map[string]string, fallback http.Handler) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		
+		path := r.URL.Path
+		if dest, ok := pathToUrls[path]; ok {
+			http.Redirect(w, r, dest, http.StatusOK)
+			return
+		}
+		fallback.ServeHTTP(w, r)
 	}
 }
